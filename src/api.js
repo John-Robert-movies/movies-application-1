f683334d84c042e4e810717c68554bdfbd5f135d
robertolago <robertolagob@gmail.com>
2018-04-25 14:24:14 -0500

    Adde some fuctions like createMovies and deleteMovies

--- src/api.js
+++ src/api.js
@@ -1,7 +1,25 @@
+import $ from "jquery";
+
 module.exports = {
   getMovies: () => {
     return fetch('/api/movies')
       .then(response => response.json());
-  }
+  },
+
+addMovies:(configuration)=>{
+    fetch('/api/movies',configuration)
+        .then(alert("movie vas created"));
+},
+
+removeMovie:(url,configuration)=>{
+    fetch(url,configuration)
+        .then(alert("movie was deleted"));
+}
+
+
+
+
+
 };
+
 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,112 @@
 /**
  * es6 modules and imports
  */
+
+import $ from "jquery";
+
 import sayHello from './hello';
 sayHello('World');
 
 /**
  * require style imports
  */
-const {getMovies} = require('./api.js');
+const {getMovies,addMovies,removeMovie} = require('./api.js');
+
+
+
+// getMovies().then((movies) => {
+//   // console.log("viendo que tiene movies");
+//   // console.log(movies);
+//   // console.log(typeof movies);
+//   console.log('Here are all the movies:');
+//   movies.forEach(({title, rating, id}) => {
+//     console.log(`id#${id} - ${title} - rating: ${rating}`);
+//   });
+// }).catch((error) => {
+//   alert('Oh no! Something went wrong.\nCheck the console for details.')
+//   console.log(error);
+// });
+
+
+
+//create the paremeters for addMovies function from api
+function create_movie(title,rating){
+const movie = {title, rating};
+const options = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(movie)
+    };
+return options;
+}
+
+//create the paremeters for removeMovies function from api
+function  remove_movie(id) {
+    let url=`/api/movies/+${id}`;
+    const options = {
+        method: 'DELETE'
+    };
+}
+
+//function to create a div with a movie
+function createReport(movie) {
+    var htmlMovie=`<tr><td>+${movie.name}+</td><<td>movie.rating</td></tr>`;
+    return htmlMovie
+
+}
+
+
+
 
 getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
+    // console.log("viendo que tiene movies");
+    // console.log(movies);
+    // console.log(typeof movies);
+    console.log('Here are all the movies:');
+    movies.forEach(({title, rating, id}) => {
+        $("#cartelera").append(`<tr><td>id#${id}</td><td>${title}</td><td>rating: ${rating}</td></tr><hr>`);
+    });
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
+    alert('Oh no! Something went wrong.\nCheck the console for details.')
+    console.log(error);
 });
+
+let peli=create_movie("casa tomada",4);
+console.log(peli);
+addMovies(peli);
+//event listener for the button "Add Movie" that create a movie taking the user inputs
+    $("#agrega").click(function () {
+        console.log($("#title"));
+        console.log($("#rating");
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
